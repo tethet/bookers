5afc0cdf773f77a5f69d36c50808bd4ecca00a4f
EC2 Default User <ec2-user@ip-172-31-35-141.ap-northeast-1.compute.internal>
2022-07-28 16:37:52 +0000

    バリデーション中

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,12 +5,17 @@ class ListsController < ApplicationController
   
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
+    @list = List.new(list_params)
+  
+    
     # 4. トップ画面へリダイレクト
-    redirect_to '/books'
+     if index.save
+      redirect_to books_path(list.id)
+     else
+      render :index
+     end  
   end
+    
 
   def index
     @lists = List.all
@@ -22,11 +27,24 @@ class ListsController < ApplicationController
   end
 
   def edit
+       @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to books_path(list.id)
+    
+  end
   
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/books'
+  end  
   def list_params
     params.require(:list).permit(:title, :body)
+  
   end
  
 end
